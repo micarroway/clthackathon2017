a02bef52f5b04b48df7b75907dc537badd215b3e
Maritza <micarroway@users.noreply.github.com>
2017-11-10 22:04:41 -0500

    tracking progress
    working on encoder

--- Hack_DecisionTreeClassifier.py
+++ Hack_DecisionTreeClassifier.py
@@ -11,6 +11,7 @@ import csv
 import pandas
 import numpy as np
 from sklearn import tree 
+from sklearn import preprocessing
 
 def __init__(self):
     pass
@@ -18,14 +19,22 @@ def __init__(self):
 claims = pandas.read_csv("C:/Users/Micar/Desktop/Hack_BH_Claims_csv.csv", sep=',', header=0)
 patients = pandas.read_csv("C:/Users/Micar/Desktop/Hack_Consumer_data_csv.csv", sep=',', header=0)
 
+#some data cleaning
+#drop columns we don't need
 claims = claims.drop(['Diagnosis','total_amount','numberofClaims'],axis=1)
-    
-#print(x)
-#print(claims)
-#print(patients)
-#with open('C:/Users/Micar/Desktop/Hack_BH_Claims_csv.csv') as csvfile:
 
-def buildmodel(claims):
+#need to convert categorical data to numbers
+le = preprocessing.LabelEncoder()
+le.fit(claims['DiagnosisCode'])    
+le.transform(claims['DiagnosisCode'])
+x = claims.iloc[:,:-1]
+y = claims.iloc[:,-1]
+x = x.apply(le.fit_transform)
+claims = pandas.concat(x,y)
+
+print(x)
+
+def buildmodel(x,y):
     #choose column with highest correlation(gender, race, ethnicity, diagnosis code, servicesummary)
     #split data set in half by rows, based on that column, for example, use average cost to split everything into two data sets above and below
     #recurse tree
@@ -34,8 +43,8 @@ def buildmodel(claims):
     #if only one column left, and one row left return answer
     #if only one column left and multiple rows left, take average
     #return new dataframe with trained estimates
-    x = claims.iloc[:,:-1]
-    y = claims.iloc[:,-1]
+    #x = claims.iloc[:,:-1]
+    #y = claims.iloc[:,-1]
     healthcareTree = tree.DecisionTreeClassifier()
     healthcareTree.fit(x,y) #getting ValueError: could not convert string to float: 'F339', need to use labelencorder, in progress
 
@@ -53,5 +62,5 @@ def searchmodel(data):
 
 if __name__ == "__main__":
 		#for testing
-		buildmodel(claims)
-    
+		#buildmodel(claims)
+        pass
