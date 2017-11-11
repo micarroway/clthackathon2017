a2b0386851028f58e1b74e226635791f741a165d
Justin Robinson <jurobn@amazon.com>
2017-11-10 22:40:51 -0500

    [master] rest api v0

--- hack/hack/urls.py
+++ hack/hack/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from .rest_framework import *
 
 urlpatterns = [
-    url(r'^api/', include('api.urls')),
-    url(r'^admin/', admin.site.urls)
+    url(r'^api/', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^admin/', admin.site.urls),
 ]
