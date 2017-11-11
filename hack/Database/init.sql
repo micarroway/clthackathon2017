d24f7e9575d62d30ffb796ea76bf9c84cc3a209b
mbalbuena <33561362+mbalbuena@users.noreply.github.com>
2017-11-10 21:52:44 -0500

    Update init.sql

--- hack/Database/init.sql
+++ hack/Database/init.sql
@@ -10,7 +10,7 @@ CREATE TABLE `api_facility` (`FacilityId` integer AUTO_INCREMENT NOT NULL PRIMAR
 --
 -- Create model FacilityAddress
 --
-CREATE TABLE `api_facilityaddress` (`FacilityAddressId` integer AUTO_INCREMENT NOT NULL PRIMARY KEY, `StreetAddress1` varchar(100) NOT NULL, `StreetAddress2` varchar(100) NOT NULL, `City` varchar(100) NOT NULL, `State` varchar(100) NOT NULL, `PostalCode` varchar(100) NOT NULL, `FacilityId_id` integer NOT NULL);
+CREATE TABLE `api_facilityaddress` (`FacilityAddressId` integer AUTO_INCREMENT NOT NULL PRIMARY KEY, `StreetAddress1` varchar(100) NOT NULL, `StreetAddress2` varchar(100) NOT NULL, `City` varchar(100) NOT NULL, `State` varchar(100) NOT NULL, `PostalCode` varchar(100) NOT NULL, `Facility_id` integer NOT NULL);
 --
 -- Create model InsuranceCompany
 --
@@ -22,39 +22,39 @@ CREATE TABLE `api_patient` (`PatientId` integer AUTO_INCREMENT NOT NULL PRIMARY 
 --
 -- Create model PatientAddress
 --
-CREATE TABLE `api_patientaddress` (`PatientAddressId` integer AUTO_INCREMENT NOT NULL PRIMARY KEY, `StreetAddress1` varchar(100) NOT NULL, `StreetAddress2` varchar(100) NOT NULL, `City` varchar(100) NOT NULL, `State` varchar(100) NOT NULL, `PostalCode` varchar(100) NOT NULL, `PatientId_id` integer NOT NULL);
+CREATE TABLE `api_patientaddress` (`PatientAddressId` integer AUTO_INCREMENT NOT NULL PRIMARY KEY, `StreetAddress1` varchar(100) NOT NULL, `StreetAddress2` varchar(100) NOT NULL, `City` varchar(100) NOT NULL, `State` varchar(100) NOT NULL, `PostalCode` varchar(100) NOT NULL, `Patient_id` integer NOT NULL);
 --
 -- Create model PatientInsurance
 --
-CREATE TABLE `api_patientinsurance` (`PatientInsuranceId` integer AUTO_INCREMENT NOT NULL PRIMARY KEY, `PremiumType` varchar(10) NOT NULL, `PremiumCost` numeric(23, 2) NOT NULL, `PatientId_id` integer NOT NULL);
+CREATE TABLE `api_patientinsurance` (`PatientInsuranceId` integer AUTO_INCREMENT NOT NULL PRIMARY KEY, `PremiumType` varchar(10) NOT NULL, `PremiumCost` numeric(23, 2) NOT NULL, `Patient_id` integer NOT NULL);
 --
 -- Create model PatientMedicalHistory
 --
-CREATE TABLE `api_patientmedicalhistory` (`PatientMedicalHistoryId` integer AUTO_INCREMENT NOT NULL PRIMARY KEY, `DateOfVisit` date NOT NULL, `CostOfVisit` numeric(23, 2) NOT NULL, `InsurancePercentage` numeric(11, 10) NOT NULL, `Diagnoses` varchar(4000) NOT NULL, `FacilityId_id` integer NOT NULL, `InsuranceCompanyId_id` integer NOT NULL, `PatientId_id` integer NOT NULL);
+CREATE TABLE `api_patientmedicalhistory` (`PatientMedicalHistoryId` integer AUTO_INCREMENT NOT NULL PRIMARY KEY, `DateOfVisit` date NOT NULL, `CostOfVisit` numeric(23, 2) NOT NULL, `InsurancePercentage` numeric(11, 10) NOT NULL, `Diagnoses` varchar(4000) NOT NULL, `Facility_id` integer NOT NULL, `InsuranceCompany_id` integer NOT NULL, `Patient_id` integer NOT NULL);
 --
 -- Create model PatientMedicalHistorySymptomDiagnosis
 --
-CREATE TABLE `api_patientmedicalhistorysymptomdiagnosis` (`id` integer AUTO_INCREMENT NOT NULL PRIMARY KEY, `DiagnosisId_id` integer NOT NULL, `PatientMedicalHistoryId_id` integer NOT NULL);
+CREATE TABLE `api_patientmedicalhistorysymptomdiagnosis` (`id` integer AUTO_INCREMENT NOT NULL PRIMARY KEY, `Diagnosis_id` integer NOT NULL, `PatientMedicalHistory_id` integer NOT NULL);
 --
 -- Create model PatientOccupation
 --
-CREATE TABLE `api_patientoccupation` (`PatientOccupationId` integer AUTO_INCREMENT NOT NULL PRIMARY KEY, `CompanyName` varchar(100) NOT NULL, `CompanyLocation` varchar(100) NOT NULL, `PositionTitle` varchar(100) NOT NULL, `Salary` numeric(23, 2) NOT NULL, `PatientId_id` integer NOT NULL);
+CREATE TABLE `api_patientoccupation` (`PatientOccupationId` integer AUTO_INCREMENT NOT NULL PRIMARY KEY, `CompanyName` varchar(100) NOT NULL, `CompanyLocation` varchar(100) NOT NULL, `PositionTitle` varchar(100) NOT NULL, `Salary` numeric(23, 2) NOT NULL, `Patient_id` integer NOT NULL);
 --
 -- Create model Symptom
 --
 CREATE TABLE `api_symptom` (`SymptomId` integer AUTO_INCREMENT NOT NULL PRIMARY KEY, `Symptom` varchar(100) NOT NULL, `SymptomDescription` varchar(4000) NOT NULL);
 --
--- Add field SymptomId to patientmedicalhistorysymptomdiagnosis
+-- Add field Symptom to patientmedicalhistorysymptomdiagnosis
 --
-ALTER TABLE `api_patientmedicalhistorysymptomdiagnosis` ADD COLUMN `SymptomId_id` integer NOT NULL;
-ALTER TABLE `api_facilityaddress` ADD CONSTRAINT `api_facilityaddress_FacilityId_id_e2fa68ac_fk_api_facil` FOREIGN KEY (`FacilityId_id`) REFERENCES `api_facility` (`FacilityId`);
-ALTER TABLE `api_patientaddress` ADD CONSTRAINT `api_patientaddress_PatientId_id_e309e5d4_fk_api_patie` FOREIGN KEY (`PatientId_id`) REFERENCES `api_patient` (`PatientId`);
-ALTER TABLE `api_patientinsurance` ADD CONSTRAINT `api_patientinsurance_PatientId_id_7ef6fc21_fk_api_patie` FOREIGN KEY (`PatientId_id`) REFERENCES `api_patient` (`PatientId`);
-ALTER TABLE `api_patientmedicalhistory` ADD CONSTRAINT `api_patientmedicalhi_FacilityId_id_6099022b_fk_api_facil` FOREIGN KEY (`FacilityId_id`) REFERENCES `api_facility` (`FacilityId`);
-ALTER TABLE `api_patientmedicalhistory` ADD CONSTRAINT `api_patientmedicalhi_InsuranceCompanyId_i_acf5a413_fk_api_insur` FOREIGN KEY (`InsuranceCompanyId_id`) REFERENCES `api_insurancecompany` (`InsuranceCompanyId`);
-ALTER TABLE `api_patientmedicalhistory` ADD CONSTRAINT `api_patientmedicalhi_PatientId_id_1d558c16_fk_api_patie` FOREIGN KEY (`PatientId_id`) REFERENCES `api_patient` (`PatientId`);
-ALTER TABLE `api_patientmedicalhistorysymptomdiagnosis` ADD CONSTRAINT `api_patientmedicalhi_DiagnosisId_id_4f39dee5_fk_api_diagn` FOREIGN KEY (`DiagnosisId_id`) REFERENCES `api_diagnosis` (`DiagnosisId`);
-ALTER TABLE `api_patientmedicalhistorysymptomdiagnosis` ADD CONSTRAINT `api_patientmedicalhi_PatientMedicalHistor_5ee86255_fk_api_patie` FOREIGN KEY (`PatientMedicalHistoryId_id`) REFERENCES `api_patientmedicalhistory` (`PatientMedicalHistoryId`);
-ALTER TABLE `api_patientoccupation` ADD CONSTRAINT `api_patientoccupatio_PatientId_id_eb547c2f_fk_api_patie` FOREIGN KEY (`PatientId_id`) REFERENCES `api_patient` (`PatientId`);
-ALTER TABLE `api_patientmedicalhistorysymptomdiagnosis` ADD CONSTRAINT `api_patientmedicalhi_SymptomId_id_79e18cec_fk_api_sympt` FOREIGN KEY (`SymptomId_id`) REFERENCES `api_symptom` (`SymptomId`);
+ALTER TABLE `api_patientmedicalhistorysymptomdiagnosis` ADD COLUMN `Symptom_id` integer NOT NULL;
+ALTER TABLE `api_facilityaddress` ADD CONSTRAINT `api_facilityaddress_Facility_id_6b683b2b_fk_api_facil` FOREIGN KEY (`Facility_id`) REFERENCES `api_facility` (`FacilityId`);
+ALTER TABLE `api_patientaddress` ADD CONSTRAINT `api_patientaddress_Patient_id_84c39b1a_fk_api_patient_PatientId` FOREIGN KEY (`Patient_id`) REFERENCES `api_patient` (`PatientId`);
+ALTER TABLE `api_patientinsurance` ADD CONSTRAINT `api_patientinsurance_Patient_id_9853e520_fk_api_patie` FOREIGN KEY (`Patient_id`) REFERENCES `api_patient` (`PatientId`);
+ALTER TABLE `api_patientmedicalhistory` ADD CONSTRAINT `api_patientmedicalhi_Facility_id_e08c7da5_fk_api_facil` FOREIGN KEY (`Facility_id`) REFERENCES `api_facility` (`FacilityId`);
+ALTER TABLE `api_patientmedicalhistory` ADD CONSTRAINT `api_patientmedicalhi_InsuranceCompany_id_82d14e57_fk_api_insur` FOREIGN KEY (`InsuranceCompany_id`) REFERENCES `api_insurancecompany` (`InsuranceCompanyId`);
+ALTER TABLE `api_patientmedicalhistory` ADD CONSTRAINT `api_patientmedicalhi_Patient_id_2673024d_fk_api_patie` FOREIGN KEY (`Patient_id`) REFERENCES `api_patient` (`PatientId`);
+ALTER TABLE `api_patientmedicalhistorysymptomdiagnosis` ADD CONSTRAINT `api_patientmedicalhi_Diagnosis_id_0f7835aa_fk_api_diagn` FOREIGN KEY (`Diagnosis_id`) REFERENCES `api_diagnosis` (`DiagnosisId`);
+ALTER TABLE `api_patientmedicalhistorysymptomdiagnosis` ADD CONSTRAINT `api_patientmedicalhi_PatientMedicalHistor_2fde7426_fk_api_patie` FOREIGN KEY (`PatientMedicalHistory_id`) REFERENCES `api_patientmedicalhistory` (`PatientMedicalHistoryId`);
+ALTER TABLE `api_patientoccupation` ADD CONSTRAINT `api_patientoccupatio_Patient_id_df8d6d3d_fk_api_patie` FOREIGN KEY (`Patient_id`) REFERENCES `api_patient` (`PatientId`);
+ALTER TABLE `api_patientmedicalhistorysymptomdiagnosis` ADD CONSTRAINT `api_patientmedicalhi_Symptom_id_dfbea790_fk_api_sympt` FOREIGN KEY (`Symptom_id`) REFERENCES `api_symptom` (`SymptomId`);
 COMMIT;
